762219bfc8d405bff4f61bf309e5b3196517d8e6
luciano nadir cazaban <thegamervlog13@gmail.com>
2021-09-02 13:36:13 -0300

    Login agregado

---
+++ main.js
@@ -0,0 +1,41 @@
+
+var MenuItems = document.getElementById("MenuItems");
+         MenuItems.style.maxHeight == "0px";
+          function menuToggle(){
+            if(MenuItems.style.maxHeight == "0px"){
+                MenuItems.style.maxHeight = "200px";
+                }
+            else{
+                MenuItems.style.maxHeight = "0px";
+                }
+          }
+
+  var productImg = document.getElementById("productImg");
+  var smallImg = document.getElementsByClassName("small-img");
+        
+        smallImg[0].onclick = function(){
+            productImg.src =  smallImg[0].src;
+        }
+        
+        smallImg[1].onclick = function(){
+            productImg.src =  smallImg[1].src;
+        }
+        
+        smallImg[2].onclick = function(){
+            productImg.src =  smallImg[2].src;
+        }
+        
+        smallImg[3].onclick = function(){
+            productImg.src =  smallImg[3].src;
+        }
+        
+
+   
+    
+    //movimiento forms
+        
+       
+
+            
+ 
+ 
